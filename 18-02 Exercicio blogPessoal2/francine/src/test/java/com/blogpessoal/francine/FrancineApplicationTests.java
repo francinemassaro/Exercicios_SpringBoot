bb6cb0b176770a442b6a567f5354b5a3a5f64734
Francine <francine.massaro@gmail.com>
2021-02-18 17:51:15 -0300

    continuando os estudos

---
+++ 18-02 Exercicio blogPessoal2/francine/src/test/java/com/blogpessoal/francine/FrancineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blogpessoal.francine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrancineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
