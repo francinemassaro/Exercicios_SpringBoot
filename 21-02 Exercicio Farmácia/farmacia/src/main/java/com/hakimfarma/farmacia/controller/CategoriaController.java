e76bce95317f6b1c06c3dcaf4eef82bf62d68f51
Francine <francine.massaro@gmail.com>
2021-02-23 15:13:57 -0300

    exercicio farmacia

---
+++ 21-02 Exercicio Farmácia/farmacia/src/main/java/com/hakimfarma/farmacia/controller/CategoriaController.java
@@ -0,0 +1,67 @@
+package com.hakimfarma.farmacia.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hakimfarma.farmacia.model.CategoriaModel;
+import com.hakimfarma.farmacia.repository.CategoriaRepository;
+
+@RestController
+@CrossOrigin ("*")
+@RequestMapping("/categorias")
+public class CategoriaController {
+	
+	@Autowired
+	private CategoriaRepository repository;
+	
+	@GetMapping
+	public ResponseEntity<List<CategoriaModel>>GetAll()
+	{
+		return ResponseEntity.ok(repository.findAll());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<CategoriaModel>GetById(@PathVariable long id)
+	{
+		return repository.findById(id).map(resp -> ResponseEntity.ok(resp))
+				.orElse(ResponseEntity.notFound().build());
+	}
+	
+	@GetMapping("/categoria/{descricao}")
+		public ResponseEntity<List<CategoriaModel>>GetByDescricao(@PathVariable String descricao){
+			return ResponseEntity.ok(repository.findAllByDescricaoContainingIgnoreCase(descricao));
+		}
+	
+	@PostMapping
+	public ResponseEntity<CategoriaModel>post(@RequestBody CategoriaModel categoria)
+	{
+		return ResponseEntity.status(HttpStatus.CREATED)
+				.body(repository.save(categoria));
+	}
+	
+	@PutMapping
+	public ResponseEntity<CategoriaModel>put(@RequestBody CategoriaModel categoria)
+	{
+		return ResponseEntity.ok(repository.save(categoria));
+	}
+	
+	@DeleteMapping("/{id}")
+	public void delete (@PathVariable long id) 
+	{
+	repository.deleteById(id);	
+	}
+	
+
+}
---
+++ 21-02 Exercicio Farmácia/farmacia/src/main/java/com/hakimfarma/farmacia/repository/ProdutoRepository.java
@@ -0,0 +1,15 @@
+package com.hakimfarma.farmacia.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.hakimfarma.farmacia.model.ProdutoModel;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository <ProdutoModel, Long> {
+	
+	public List<ProdutoModel> findAllByNomeContainingIgnoreCase(String nome);
+
+}
